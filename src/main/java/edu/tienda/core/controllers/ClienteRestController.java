d408ff5905cf77c713d606a79b6ec76e17ca7abe
Miguel Nequiz <nekixito@gmail.com>
2024-02-21 19:20:15 -0600

    Controladores para obtener clientes y para un cliente por username

--- src/main/java/edu/tienda/core/controllers/ClienteRestController.java
+++ src/main/java/edu/tienda/core/controllers/ClienteRestController.java
@@ -1,2 +1,45 @@
-package edu.tienda.core.controllers;public class ClienteRestController {
+package edu.tienda.core.controllers;
+
+import edu.tienda.core.domain.Cliente;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class ClienteRestController {
+
+    private List<Cliente> clientes = new ArrayList<>(Arrays.asList(
+            new Cliente("arm","1234","Armstrong"),
+            new Cliente("ald","1234","Aldrin"),
+            new Cliente("col","1234","Collins")
+    ));
+
+    @GetMapping("/clientes")
+    public List<Cliente> getClientes(){
+        return clientes;
+    }
+
+    @GetMapping("/clientes/{userName}")
+    public Cliente getCliente(@PathVariable String userName){
+
+        return clientes.stream().filter(cliente -> cliente.getUsername().equalsIgnoreCase(userName)).findFirst().orElseThrow();
+
+        /*
+        for (Cliente cli : clientes){
+            if (cli.getUsername().equalsIgnoreCase(userName)){
+                return cli;
+            }
+        }
+
+        return null;
+
+         */
+    }
+
+
+
 }
