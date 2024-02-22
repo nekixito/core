d15aaef6ec24f5a7e3616220061775a4af36d194
Miguel Nequiz <nekixito@gmail.com>
2024-02-21 18:48:21 -0600

    Se pone una saludo personalizado en HolaMundoRestController

--- src/main/java/edu/tienda/core/controllers/HolaMundoRestController.java
+++ src/main/java/edu/tienda/core/controllers/HolaMundoRestController.java
@@ -1,14 +1,15 @@
 package edu.tienda.core.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HolaMundoRestController {
 
-    @GetMapping("/hola")
-    public String saludo(){
+    @GetMapping("/hola/{nombreUsuario}")
+    public String saludo(@PathVariable String nombreUsuario){
         System.out.println("Ejecutando el metodo saludo");
-        return "Hola Mundo Spring Boot";
+        return "Hola " + nombreUsuario;
     }
 }
