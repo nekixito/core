2f6fbe80c71d5a5e29b7780af7d3317d01b96db5
Miguel Nequiz <nekixito@gmail.com>
2024-03-07 10:58:15 -0600

    Se crea Producto, se separa en domain, service y controller para obtener productos

---
+++ src/main/java/edu/tienda/core/controllers/ProductosControllerRest.java
@@ -0,0 +1,25 @@
+package edu.tienda.core.controllers;
+
+import edu.tienda.core.domain.Producto;
+import edu.tienda.core.services.ProductosServiceImpl;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/productos")
+public class ProductosControllerRest {
+
+    private ProductosServiceImpl productosService = new ProductosServiceImpl();
+
+    @GetMapping
+    public ResponseEntity<?> getProductos(){
+
+        List<Producto> productos = productosService.getProductos();
+
+        return ResponseEntity.ok(productos);
+    }
+}
