4bbb0672f175fda1089fe71a548d85a07dfaef33
Miguel Nequiz <nekixito@gmail.com>
2024-03-21 16:41:53 -0600

    Uso de JPA Wildcards para queries

--- src/main/java/edu/tienda/core/presistance/repositories/ProductoRepository.java
+++ src/main/java/edu/tienda/core/presistance/repositories/ProductoRepository.java
@@ -4,6 +4,14 @@ import edu.tienda.core.presistance.entities.ProductoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProductoRepository extends JpaRepository<ProductoEntity,Integer> {
+
+    List<ProductoEntity> findByPrecioLessThan(Double precio);
+
+    List<ProductoEntity> findByNombreLike(String nombre);
+
+    List<ProductoEntity> findByPrecioGreaterThanAndStockLessThan(Double precio,Integer stock);
 }
